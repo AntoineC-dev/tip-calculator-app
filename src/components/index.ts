366c2dce387179e276916cd6cdb014fd3317a8f2
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-12 18:12:09 -0400

    Export new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Header } from './header/Header';
+export { default as Input } from './input/Input';
+export { default as InputWrapper } from './inputWrapper/InputWrapper';
+export { default as RadioButton } from './radioButton/RadioButton';
