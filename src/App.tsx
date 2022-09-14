58dea113f2455351d472f6c121b7a3999fc61390
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-13 21:25:03 -0400

    Create dense props on Input

--- src/App.tsx
+++ src/App.tsx
@@ -51,6 +51,7 @@ const App: Component = () => {
                 Custom percentage
               </label>
               <Input
+                dense={true}
                 error={s.percentage.error}
                 value={s.customPercentage.value}
                 setValue={setValue('customPercentage')}
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -5,13 +5,14 @@ import styles from './Input.module.css';
 interface InputProps extends Omit<ComponentProps<'input'>, 'class' | 'type'> {
   error?: string;
   icon?: string;
+  dense?: boolean;
   setValue: (value: string) => void;
 }
 
 const Input: Component<InputProps> = (props: InputProps) => {
-  const [local, rest] = splitProps(props, ['error', 'icon', 'setValue']);
+  const [local, rest] = splitProps(props, ['error', 'icon', 'setValue', 'dense']);
   return (
-    <div classList={{ [styles.container]: true, [styles.error]: !!local.error }}>
+    <div classList={{ [styles.container]: true, [styles.error]: !!local.error, [styles.dense]: local.dense }}>
       <Show when={local.icon}>
         <img src={local.icon} alt="" class={styles.icon} />
       </Show>
